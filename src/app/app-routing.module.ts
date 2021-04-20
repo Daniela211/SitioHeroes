26c4b81795d7a658f6ea828cedb8144735fa4fa8
Daniela21 <201821158@tese.edu.mx>
2021-04-19 21:36:56 -0500

    Se creó el botón eliminar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
+
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
 
-@NgModule({
+
+ const routes: Routes = [];
+
+
+
+ @NgModule({
+
   imports: [RouterModule.forRoot(routes)],
+
   exports: [RouterModule]
+
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule { 
+
+  
+
+}
